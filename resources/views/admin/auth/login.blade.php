0ebe454df0b368ef19c6187a4be88ec0f3e3159f
coreoceantesting <coreoceantesting@gmail.com>
2024-08-20 14:32:17 +0530

    change title of login page

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>TMC | Login Page</title>
+        <title>SRA</title>
         <link rel="shortcut icon" href="{{ asset('admin/images/Group 1.png') }}">
         <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
