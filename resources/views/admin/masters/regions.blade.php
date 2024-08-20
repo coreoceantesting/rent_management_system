18a2450a7a98b5b4527277a08267c8843bba9f97
coreoceantesting <coreoceantesting@gmail.com>
2024-08-20 17:04:04 +0530

    tbody close tag

--- resources/views/admin/masters/regions.blade.php
+++ resources/views/admin/masters/regions.blade.php
@@ -111,6 +111,7 @@
                                             </td>
                                         </tr>
                                     @endforeach
+                                </tbody>
                             </table>
                         </div>
                     </div>
