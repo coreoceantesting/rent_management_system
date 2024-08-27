d4b88783fb3067a9d98a810706bcc0145dd86491
coreoceantesting <coreoceantesting@gmail.com>
2024-08-27 18:07:33 +0530

    fevicon added

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SRA</title>
-        <link rel="shortcut icon" href="{{ asset('admin/images/Group 1.png') }}">
+        <link rel="shortcut icon" href="{{ asset('admin/images/Group 1 copy 2.png') }}">
         <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
