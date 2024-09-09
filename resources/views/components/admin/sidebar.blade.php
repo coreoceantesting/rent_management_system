474979151ee444872bce6eb40ac3aa9443748895
coreoceantesting <coreoceantesting@gmail.com>
2024-09-09 16:29:56 +0530

    change minor bug in sidebar logo

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -16,7 +16,7 @@
                 <img src="{{ asset('admin/images/sidebarlogo3.png') }}" alt="" height="35" />
             </span>
             <span class="logo-lg">
-                <img src="{{ asset('admin/images/sidebarlogo3.png') }}" alt="" height="35   " />
+                <img src="{{ asset('admin/images/sidebarlogo3.png') }}" alt="" height="35" />
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
